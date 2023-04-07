7b4d7a418f86acffb9b8bbe54d11bf2e9aa8b4ad
Roman Orlov <helltester666@gmail.com>
2023-04-07 11:32:03 +0200

    add scripts

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env sh
 
-set -e
 npm run build
-cd dist
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:orlovrs/time-tracker.git main:gh-pages
-
-cd -
+git add dist -f
+git commit -m 'adding dist'
+git subtree push --prefix dist origin gh-pages
